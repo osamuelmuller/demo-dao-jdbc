12ea6b2f99ff138bf1b2ba9dfcc570ca324348b4
Samuel Muller <samuelmuller.login@gmail.com>
2026-05-16 09:39:02 -0300

    Reusing Instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,12 @@ public class Program {
     static void main() {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1 : seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("=== TEST 2 : findByDepartmentId ===");
+        Department department = sellerDao.findByDepartmentId();
     }
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -52,17 +52,8 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
 
             if (rs.next()) {
-                Department department = new Department();
-                department.setId(rs.getInt("DepartmentId"));
-                department.setName(rs.getString("DepName"));
-                Seller seller = new Seller();
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
-                seller.setDepartment(department);
-                return seller;
+                Department department = initiateDepartment(rs);
+                Seller seller = initiateSeller(rs, department);
             }
             return null;
         }
@@ -73,6 +64,24 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    private Department initiateDepartment (ResultSet rs) throws SQLException {
+        Department department = new Department();
+        department.setId(rs.getInt("DepartmentId"));
+        department.setName(rs.getString("DepName"));
+        return department;
+    }
+
+    private Seller initiateSeller (ResultSet rs, Department department) throws SQLException {
+        Seller seller = new Seller();
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
+        seller.setDepartment(department);
+        return seller;
     }
 
     @Override
